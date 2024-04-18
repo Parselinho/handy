a9f8ba7aecb8da6d8c1c233de3931963d73964cf
Parselinho <parselinho@gmail.com>
2024-04-18 21:33:18 +0300

    homepage-progress

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -2,19 +2,22 @@ import { FaSearch } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <div className="hero bg-base-200">
+    <div className="hero bg-base-200 flex flex-col items-center">
       <div className="hero-content">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold mb-5">
+          <h1 className="text-5xl font-bold mb-1">
             Find your trusted help for home tasks
           </h1>
-          <label className="input input-bordered flex items-center gap-2">
-            <input type="text" className="grow" placeholder="Search" />
-            <button>
-              <FaSearch />
-            </button>
-          </label>
         </div>
+      </div>
+      <div className="join w-10/12 m-1 mr-2 ml-2 rounded-3xl p-1">
+        <input
+          className="input input-bordered join-item w-full "
+          placeholder="What do you need help with?"
+        />
+        <button className="btn join-item rounded-r-full bg-green-600 w-20 text-white">
+          <FaSearch />
+        </button>
       </div>
     </div>
   );
--- src/components/home/TaskItem.tsx
+++ src/components/home/TaskItem.tsx
@@ -11,14 +11,14 @@ const TaskItem = ({
       style={{ flexBasis: "105px" }}
     >
       <div
-        className="relative flex flex-col justify-center"
-        style={{ height: "46px" }}
+        className="relative flex flex-col justify-center items-center"
+        style={{ height: "75px" }}
       >
         <Icon style={{ height: "50", width: "60" }} />
-        <span className="leading-none tracking-normal">
+        <span className="leading-none tracking-normal ">
           <label
-            className="m-0 font-bold text-xs tracking-tighter leading-snug text-center flex shrink-0 flex-grow-1 pl-2 pr-2 cursor-pointer"
-            style={{ flexBasis: "40px" }}
+            className="m-0 mt-5 font-bold text-xs tracking-tighter leading-snug text-center flex shrink-0 flex-grow-1 pl-2 pr-2 cursor-pointer"
+            style={{ flexBasis: "40px", fontSize: "12px" }}
           >
             {label}
           </label>
